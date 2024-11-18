b0687699a9d69e30887c1a1bbde688c0e15c9897
aruna devi <queenarunadevi.a@gmail.com>
2024-11-18 21:09:42 +0530

    flask task

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from flask import Flask, session, request, redirect, url_for, render_template
 import secrets
 
-
-
-
-
-
-
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(8)
 
@@ -24,32 +18,29 @@ def home():
 @app.route('/profile')
 def profile(): 
     if "user_name" in session :
-       
         user_name = session.get("user_name")
         if user_name in users:
             users={
                 "name" :user_name, 
                 "city"  : users[user_name]
             }
-        return render_template('profile.html', user_name = users )
-    return redirect (url_for("login"))
+    return render_template('profile.html', user_name = users )
+    #return redirect (url_for("login"))
        
 
 @app.route('/login', methods = ['GET','POST'])
 def login():
     if request.method == "POST":
         user_name = request.values.get("user_name")
+        print(f"name:{user_name}")
         if user_name in users:
-
             session['name'] = user_name
-        
-         
             print(f"session : {session}")
             return redirect(url_for("profile"))
-        else:
-            error = "invalid user_name"
-               
-    return render_template('login.html', error= error)
+    return render_template('login.html')
+    #else:
+            #return "invalid user_name"
+        
 
 @app.route('/logout')
 def session_logout():
